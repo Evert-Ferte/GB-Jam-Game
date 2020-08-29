9e18ce3f770a930c02339f47adf873f850b8c501
Mart Foekema <m.foekema@outlook.com>
2020-08-30 00:21:32 +0200

    Work in progress

---
+++ Assets/01Scripts/SimonSaysController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimonSaysController : MonoBehaviour
+{
+    [SerializeField] private List<SpriteRenderer> tiles = new List<SpriteRenderer>();
+    [SerializeField] private List<Color> palette = new List<Color>();
+
+    public void TileClicked(SpriteRenderer sprite)
+    {
+
+    }
+}
+
---
+++ Assets/01Scripts/SimonSaysTile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimonSaysTile : MonoBehaviour
+{
+    public SimonSaysController controller;
+    private SpriteRenderer renderer;
+
+    private void Awake() 
+    {
+        renderer = GetComponent<SpriteRenderer>();    
+    }
+
+    private void OnMouseDown() 
+    {
+        controller.TileClicked(renderer);
+    }
+}
