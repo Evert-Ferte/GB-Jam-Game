9680ad3d3264f9583f498b475f84ae29db8b406a
Evert de la Ferté <evertferte@hotmail.com>
2020-08-30 00:29:55 +0200

    Started working on the terminal component.
    Lines now popup on the top or bottom of the screen.

---
+++ Assets/01Scripts/Terminal/TerminalController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Terminal {
+    public class TerminalController : MonoBehaviour {
+        public bool textFromBottom = true;
+        [Space]
+        
+        public Transform lineHolder;
+        public GameObject terminalTextLinePrefab;
+        
+        private List<string> lines = new List<string>();
+        
+        void Start() {
+            
+        }
+
+        void Update() {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                AddLine("hello world");
+            }
+        }
+
+        private void AddLine(string newLine) {
+            // TODO - check if text overflows, then add multiple lines instead of 1 long line
+            
+            // Add the line to a list of lines
+            lines.Add(newLine);
+
+            // Spawn the lines
+            Vector3 pos = new Vector3(0, -45 + 45 * lines.Count, 0);
+            if (!textFromBottom)
+                pos = new Vector3(0, 483 - (-45 + 45 * lines.Count), 0);
+            
+            TerminalTextLine line = Instantiate(terminalTextLinePrefab, Vector3.zero, Quaternion.identity, lineHolder).GetComponent<TerminalTextLine>();
+
+            // Apply the correct position
+            line.GetComponent<RectTransform>().anchoredPosition = pos;
+            
+            // Set the line number and text
+            line.SetLineNumber(lines.Count);
+            line.SetLineText(newLine);
+        }
+    }
+}
---
+++ Assets/01Scripts/Terminal/TerminalTextLine.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.Terminal {
+    public class TerminalTextLine : MonoBehaviour {
+        public Text lineNumber;
+        public Text lineText;
+        
+        public void SetLineNumber(int number) {
+            lineNumber.text = number.ToString();
+        }
+
+        public void SetLineText(string text) {
+            lineText.text = text;
+        }
+    }
+}
