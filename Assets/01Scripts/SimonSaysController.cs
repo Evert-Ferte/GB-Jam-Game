5b6e84ef5fe22db1e100447752e9067dff14c195
Mart Foekema <m.foekema@outlook.com>
2020-09-06 19:48:09 +0200

    Added simple sound effects.

--- Assets/01Scripts/SimonSaysController.cs
+++ Assets/01Scripts/SimonSaysController.cs
@@ -7,6 +7,7 @@ public class SimonSaysController : MonoBehaviour
 {
 	[SerializeField] private List<SimonSaysTile> tiles = new List<SimonSaysTile>();
 	[SerializeField] private int maxSequence = 0;
+	[SerializeField] AudioSource audioSource = null;
 	private List<SimonSaysTile> gameSequence = new List<SimonSaysTile>();
 	public bool SequencePlaying { get; private set; }
 	private int sequenceID = 0;
@@ -21,6 +22,10 @@ public class SimonSaysController : MonoBehaviour
 		SimonSaysTile tile = tiles[id];
 
 		if (SequencePlaying) return;
+
+		audioSource.clip = BeepBoop.GetTone(1000, 44000, Mathf.FloorToInt(Random.Range(440, 550)));
+		audioSource.Play();
+
 		if (tile != gameSequence[sequenceID]) // Wrong tile has been pressed.
 		{
 			StartCoroutine(ResetSequence());
@@ -69,6 +74,8 @@ public class SimonSaysController : MonoBehaviour
 		for (int i = 0; i < gameSequence.Count; i++)
 		{
 			yield return new WaitForSeconds(.5f);
+			audioSource.clip = BeepBoop.GetTone(10000, 44000, 1000);
+			audioSource.Play();
 			gameSequence[i].UpdateColor(true);
 			yield return new WaitForSeconds(.5f);
 			gameSequence[i].UpdateColor(false);
