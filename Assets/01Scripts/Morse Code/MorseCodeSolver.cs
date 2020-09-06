3f1f97c5d4719abe662f594bc05feabefdd65274
Mart Foekema <m.foekema@outlook.com>
2020-09-06 17:38:30 +0200

    Added solution letters in the decoder page.

--- Assets/01Scripts/Morse Code/MorseCodeSolver.cs
+++ Assets/01Scripts/Morse Code/MorseCodeSolver.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Game;
+using TMPro;
 
 public class MorseCodeSolver : MonoBehaviour
 {
     [SerializeField] private GameObject solutionPath = null;
     [SerializeField] private string solution = null;
     [SerializeField] private string guess = "";
+    [SerializeField] List<TextMeshProUGUI> boxes = new List<TextMeshProUGUI>();
 
     public void AddLetter(string letter)
     {
         if (guess.Length < solution.Length) 
         {
             guess += letter;
+            char[] letters = guess.ToCharArray();
+            for (int i = 0; i < letters.Length; i++){
+                boxes[i].text = letters[i].ToString();
+            }
             if (guess.Length == solution.Length) CheckGuess();
         }
     }
@@ -51,6 +57,9 @@ public class MorseCodeSolver : MonoBehaviour
         solutionPath.SetActive(false);
         yield return new WaitForSeconds(.25f);
         solutionPath.SetActive(true);
+        for (int i = 0; i < boxes.Count; i++){
+            boxes[i].text = string.Empty;
+        }
     }
 
     private IEnumerator Scroll()
